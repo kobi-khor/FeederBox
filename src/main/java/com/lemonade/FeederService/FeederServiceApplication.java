6590f50edb445c5725e45125c344e30a50d27183
kobi-khor <57593714+kobi-khor@users.noreply.github.com>
2022-09-03 01:15:20 +0300

    first commit- files added

---
+++ src/main/java/com/lemonade/FeederService/FeederServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lemonade.FeederService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeederServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeederServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lemonade/FeederService/FeederServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lemonade.FeederService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeederServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
