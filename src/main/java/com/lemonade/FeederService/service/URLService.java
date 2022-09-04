e8b5dd536e0131fa8e1a27762e72f05b678859e2
kobi-khor <57593714+kobi-khor@users.noreply.github.com>
2022-09-04 23:21:32 +0300

    code modefied

--- src/main/java/com/lemonade/FeederService/service/URLService.java
+++ src/main/java/com/lemonade/FeederService/service/URLService.java
@@ -2,11 +2,13 @@ package com.lemonade.FeederService.service;
 
 import com.lemonade.FeederService.models.URL;
 import com.lemonade.FeederService.repository.URLRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class URLService {
 
+    @Autowired
     URLRepository urlRepository;
 
     /**
