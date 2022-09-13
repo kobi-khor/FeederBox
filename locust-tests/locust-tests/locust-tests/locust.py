f25fdcd775df71665cde024eb73530610bdb9e5a
kobi-khor <57593714+kobi-khor@users.noreply.github.com>
2022-09-13 20:58:32 +0300

    locust tests modified. working

--- locust-tests/locust-tests/locust-tests/locust.py
+++ locust-tests/locust-tests/locust-tests/locust.py
@@ -1,9 +1,19 @@
-
 from locust import HttpUser, SequentialTaskSet, between, task
 import json
+import csv
 
 
 class User(HttpUser):
+
+    global data
+    # if (data == None):
+    #     with open('top500domains.csv', 'r') as l:
+    #         reader = csv.reader(l)
+    #         data = list(reader)
+    with open('top500domains.csv', 'r') as l:
+        reader = csv.reader(l)
+        data = list(reader)
+
     @task
     class SequenceOfTasks(SequentialTaskSet):
         wait_time = between(1, 5)
@@ -14,12 +24,12 @@ class User(HttpUser):
 
         @task
         def post(self):
+            if len(data) > 0:
+                self.my_value = data.pop()[0]
             self.client.post("/", json=
             {
-                 "url": "https://www.Arseal.com"
+                "url": "https://" + self.my_value
             }
-            )
-
+                             )
 
 #   locust --host=http://localhost:8085 --locustfile locust.py
-
