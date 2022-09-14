6323babe2106d62791dab6e647f5fc364402d020
kobi-khor <57593714+kobi-khor@users.noreply.github.com>
2022-09-14 20:53:09 +0300

    modified

--- URLFeederService/src/main/java/com/lemonade/FeederService/common/Constants.java
+++ URLFeederService/src/main/java/com/lemonade/FeederService/common/Constants.java
@@ -4,5 +4,5 @@ package com.lemonade.FeederService.common;
  * constants to be used in package
  */
 public class Constants {
-    public static final String URL_UUD_PREFIX = "url:";
+    public static final String URL_UUID_PREFIX = "url:";
 }
